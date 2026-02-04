a45ab9d3477d175742f4f73eafcebacb6a25731a
EPICSTYLE3 <mm0326007@gmail.com>
2026-02-04 20:15:19 +0000

    fixed the logic and now it works :)

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int checkcomplete(int puzzle[]){
     return(0);
 
 }
-int* solve(int puzzle[]){
+int solve(int puzzle[], int broken){
     int i;
     int m;
     int a;
@@ -41,38 +41,47 @@ int* solve(int puzzle[]){
     {
         if (puzzle[i]==0) //if unsolved
         {
+            printf("\n --------------- starting new comp in positon %d\n",i);
             int possible[9] = {1,2,3,4,5,6,7,8,9};
-            row = floor(i/9);
+            row = i/9;
             column = i%9; //starting at 0->8
+            // printf("\n number %d Row %d Col %d",i, row,column);
             // ----------------- box logic
+            // required maybe
             for(a=0;a<3;a++) //row of box
                 {
                     for(b=0;b<3;b++) //col of box
                     {
                         box = (b+(column- (column%3))+((row- (row%3)+a)*9));
-                        boxcomp[a*3+b] = puzzle[(b+(column- column%3)+((row- row%3+a)*9))];   // might be brk
+                        boxcomp[(a*3)+b] = puzzle[(b+(column- column%3))+((row- row%3+a)*9)];   // might be brk
                     }
                 } 
             //---------------------------
+            
             for (m=0;m<9;m++) //check row for impossible options
             {
-                rowvalue = puzzle[(m+9*row)]; 
+                rowvalue = puzzle[(m+(9*row))]; 
                 if (rowvalue !=0)
                 {
+                    //printf("\n rowvalue %d detected deleting %d was compared to pos%d",rowvalue, possible[rowvalue-1], (m+(9*row)));
                     possible[rowvalue-1]=0;
                 }
 
-                columnvalue = puzzle[column+m*9];
-                if (columnvalue!=0);
+                columnvalue = puzzle[column+(m*9)];
+                if (columnvalue != 0)
                 {
+                    //printf("\n col %d detected deleting %d",columnvalue, possible[columnvalue-1]);
                     possible[columnvalue-1]=0;
+                    
                 }
+                
                 if (boxcomp[m] !=0)
                 {
+                    // printf("\n box numbers = %d detected deleting %d",boxcomp[m], possible[boxcomp[m]-1]);
                     possible[boxcomp[m]-1]=0;
                 }
+                
             }
-
             // insert logic to sub in the numbers to puzzle if theres only one possible
             int possibleoptions =0;
             int possiblepos;
@@ -87,17 +96,18 @@ int* solve(int puzzle[]){
             if (possibleoptions==1) // if there is only one possible option
             {
                 puzzle[i] = possible[possiblepos];
-                // printf("\n made a change");
+                // printf("\n made a change Number %d became %d",i,possible[possiblepos]);
+                broken =0;
 
             }
         }
        
     }
-    return puzzle;
+    return broken;
 }
-int *write(int done[])
+void write(int done[])
 {
-    int printhelp;
+    int printhelp = 0;
     int i;
     char writing;
     fileptr =fopen("solved.txt","w");
@@ -106,9 +116,9 @@ int *write(int done[])
         writing = ("%d ", done[i]);
         fprintf(fileptr,"%d ",done[i]);
         printhelp++;
-        if (printhelp ==9)
+        if (printhelp == 9)
         {
-            fprintf(fileptr,"\n");
+            fprintf(fileptr," \n");
             printhelp =0;
         }
     }
@@ -122,26 +132,26 @@ int main(void){
     starting(puzzle);
     int i;
     int j;
+    int broken = 0;
     int new[81]= {0};
     for (i=0; i<81;i++)
     {
         new[i] = puzzle[i];
+        printf("%d ",new[i]);
     }
     int cont = 1;
-    int broken =0;
 //---------------Main LOOP---------------
     while (cont == 1 && broken != 1)       
     {
-        solve(new);
+        broken = solve(new ,1);
         cont = checkcomplete(new);
-        broken = 1;
         // check and see if changes were made
         for (j=0; j<81;j++);
-            if (new[i] != puzzle[i])
+            if (new[j] != puzzle[j])
             {
-                printf("this happened");
+                printf("  <- but meaningful change");
                 broken = 0; // a change was made
-                puzzle[i] = new[i];
+                puzzle[j] = new[j];
             }
     }
     //----------------------------------------
@@ -155,7 +165,8 @@ int main(void){
     if (broken ==1){
         printf("\n Couldnt solve too stupid :( ");
     }
-    else{
+//    else{
+    printf("\n");
         for (i = 0; i < 81; i++)
         {
             printf("%d ", new[i]);
@@ -166,7 +177,7 @@ int main(void){
                 printhelp =0;
             }
         }
-    }
+//    }
     write(new);
     return 0;
 }
